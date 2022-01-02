dd2a29091a8959a317744c7b89b0208cfb5db3ef
ryoryoryoooo <P1211065@gmail.com>
2022-01-02 22:50:42 +0900

    Initial Commit

---
+++ ProgressView2022-01-02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgressView2022-01-02
+//
+//  Created by 村中令 on 2022/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
